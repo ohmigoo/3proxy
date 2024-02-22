481a3873d3f44241307af65c5ee55102d6431f9c
kim <146644180+jwheeler00662@users.noreply.github.com>
2024-02-22 19:21:13 +0700

    Update console.js

--- console.js
+++ console.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const axios = require('axios')
+
 const app = express();
 const PORT = 3000;
+
+
+app.use('/', async (req, res) => {
+    try {
+        const response = await axios.get('https://example.com' + req.url);
+        res.send(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Proxy request failed');
+    }
+});
